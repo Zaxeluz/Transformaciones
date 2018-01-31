a2cfb8417a1dc9ca5e0e136bec134311d81f9a03
René <amarillas_rene@hotmail.com>
2018-01-31 11:48:19 -0700

    Transformaciones

--- Graficos/Graficos.cpp
+++ Graficos/Graficos.cpp
@@ -12,11 +12,19 @@
 using namespace std;
 
 GLfloat red, green, blue;
+GLfloat angulo = 0.0f;
 
 void actualizar() { 
 	//Aquí esta bien para cambiar los valores
 	//De las variables de mi programa!
-	
+
+	if (angulo < 360){
+		angulo += 0.01;
+	}else
+	{
+		angulo = 0;
+	}
+
 	/*red += 0.001;
 	green += 0.002;
 	blue += 0.003;
@@ -28,18 +36,26 @@ void actualizar() {
 }
 
 void dibujar() {
+	glPushMatrix();
+	glTranslatef(0.2f, -0.2f, 0.0f);
+	//glRotatef(45.0f, 1.0f, 0.0f, 0.0f); //Afecta eje x
+	//glRotatef(angulo, 0.0f, 1.0f, 0.0f); //Afecta eje y
+	glRotatef(angulo, 0.0f, 0.0f, 1.0f); //Afecta eje z
+	glScalef(0.2f, 0.2f, 0.2f);
+
 	glBegin(GL_TRIANGLES); //Inicia la rutina con un modo de dibujo
 
 	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(-1.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, -0.5f, 0.0f);
 
 	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 0.5f, 0.0f);
 
 	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(1.0, 0.0f, 0.0f);
+	glVertex3f(1.0, -0.5f, 0.0f);
 
 	glEnd();//Finaliza la rutina
+	glPopMatrix();
 }
 
 
